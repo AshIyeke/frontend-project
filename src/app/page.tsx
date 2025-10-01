7658b9614c1ed38e0a7e614ca597e628997d9ccc
ashioma <ashiomaiyeke8@gmail.com>
2025-10-01 22:35:25 +0100

    nigth review

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
+"use client";
+import dynamic from "next/dynamic";
+import Features from "@/components/Features"
 import HeroSection from "@/components/HeroSection"
 import Search from "@/components/Search"
+// import AnimatedTestimonialsDemo from "@/components/Testimonials"
+import { Animation } from "react-day-picker"
 
 
 export default function Home() {
+  const AnimatedTestimonialsDemo = dynamic(
+    () => import("../components/Testimonials").then((mod) => mod.default),
+    { ssr: false }
+  );
   return (
     <div className="container mx-auto">
       <HeroSection/>
       <Search/>
+      <Features/>
+      <AnimatedTestimonialsDemo/>
     </div>
   )
 }
